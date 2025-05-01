f193fb6aa84bf9d40cb84cfcc4c39f3f90621b61
csw <zobirofkir19@gmail.com>
2025-05-01 15:45:35 +0100

    add passport auth service in folder name Auth

--- app/Http/Controllers/Auth/PassportAuthController.php
+++ app/Http/Controllers/Auth/PassportAuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Services\PassportAuthService;
+use App\Services\Auth\PassportAuthService;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Requests\Auth\SendLoginLinkRequest;
--- app/Services/Auth/PassportAuthService.php
+++ app/Services/Auth/PassportAuthService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Auth;
 
 use App\Models\User;
 use Illuminate\Support\Str;
