c098bc7f0dce4cba88a104009964703053fa7120
csw <zobirofkir19@gmail.com>
2025-05-03 21:04:04 +0100

    clean api.php code

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,6 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\EmailAuthController;
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::middleware('auth:api')->group(function () {
-    Route::get('/auth/user', [EmailAuthController::class, 'getUser']);
-});
 
 
-Route::prefix('/auth')->group(function() {
-    Route::post('/send-login-link', [EmailAuthController::class, 'sendLoginLink']);
-    Route::post('/verify-login', [EmailAuthController::class, 'verifyLogin']);
-    Route::post('/send-otp', [EmailAuthController::class, 'sendOTP']);
-    Route::post('/verify-otp', [EmailAuthController::class, 'verifyOTP']);
-    Route::post('/login', [EmailAuthController::class, 'login']);
-});
+
+require __DIR__.'/auth/mail.php';
