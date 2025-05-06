2e7492a1080133fea8dfe712a8762fe7e5cbaee1
csw <zobirofkir19@gmail.com>
2025-05-06 11:20:09 +0100

    change phone

--- routes/auth/phone.php
+++ routes/auth/phone.php
@@ -2,6 +2,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\PhoneAuthController;
 
-Route::post('/send-otp', [PhoneAuthController::class, 'sendOTP']);
-Route::post('/verify-otp', [PhoneAuthController::class, 'verifyOTP']);
-Route::post('/login', [PhoneAuthController::class, 'login']);
+Route::prefix('auth/phone')->group(function () {
+    Route::post('/send-otp', [PhoneAuthController::class, 'sendOTP']);
+    Route::post('/verify-otp', [PhoneAuthController::class, 'verifyOTP']);
+    Route::post('/login', [PhoneAuthController::class, 'login']);
+});
