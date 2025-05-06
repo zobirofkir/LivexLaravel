fccceba6cfae56e09f8da1b92f9c0eb292b51c5d
csw <zobirofkir19@gmail.com>
2025-05-06 15:08:25 +0100

    move AuthUserController to Auth folder

--- routes/auth/authenticated.php
+++ routes/auth/authenticated.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthUserController;
+use App\Http\Controllers\Auth\AuthUserController;
 
 Route::middleware('auth:api')->prefix('auth/user')->group(function () {
     Route::put('/update', [AuthUserController::class, 'update']);
