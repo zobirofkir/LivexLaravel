3305c1191e7fd790f7e0398b0041de8a43af9d2e
csw <zobirofkir19@gmail.com>
2025-05-10 06:07:08 +0100

    create logic for Video Controller

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,9 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\VideoRequest;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\VideoResource;
+use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class VideoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return VideoResource::collection(
+            Video::all()
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @return VideoResource
+     */
+    public function show(Video $video) : VideoResource
+    {
+        return VideoResource::make($video);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param VideoRequest $request
+     * @param Video $video
+     * @return VideoResource
+     */
+    public function store(VideoRequest $request) : VideoResource
+    {
+        return VideoResource::make(
+            Video::create($request->validated())
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param VideoRequest $request
+     * @param Video $video
+     * @return VideoResource
+     */
+    public function update(VideoRequest $request, Video $video) : VideoResource
+    {
+        $video->update($request->validated());
+
+        return VideoResource::make(
+            $video->refresh()
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param Video $video
+     * @return bool
+     */
+    public function destroy(Video $video) : bool
+    {
+        return $video->delete();
+    }
+    
 }
