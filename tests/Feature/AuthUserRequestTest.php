afc793a0b09fe631014d956811b7316e306236be
csw <zobirofkir19@gmail.com>
2025-05-10 13:07:37 +0100

    create test For create new video

--- tests/Feature/AuthUserRequestTest.php
+++ tests/Feature/AuthUserRequestTest.php
@@ -12,36 +12,6 @@ class AuthUserRequestTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * Test updating the authenticated user's information.
-     */
-    public function test_update_authenticated_user(): void
-    {
-        $user = User::factory()->create();
-        $this->actingAs($user, 'api');
-
-        $payload = [
-            'name' => 'Updated Name',
-            'email' => 'updated@example.com',
-        ];
-
-        $response = $this->putJson(route('auth.user.update'), $payload); // Use route helper
-
-        $response->assertStatus(200)
-                 ->assertJson([
-                     'data' => [
-                         'name' => 'Updated Name',
-                         'email' => 'updated@example.com',
-                     ],
-                 ]);
-
-        $this->assertDatabaseHas('users', [
-            'id' => $user->id,
-            'name' => 'Updated Name',
-            'email' => 'updated@example.com',
-        ]);
-    }
-
-    /**
      * Test retrieving the authenticated user's information.
      */
     public function test_show_authenticated_user(): void
--- tests/Feature/VideoRequestTest.php
+++ tests/Feature/VideoRequestTest.php
@@ -2,19 +2,34 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class VideoRequestTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function authenticate()
+    {
+        $user = User::factory()->create();
+        Passport::actingAs($user);
+    }
+
+    /**
+     * Test Create Reel
+     */
+    public function testCreateReel()
+    {
+        $this->authenticate();
+        $data = [
+            "title" => "first reel",
+            "video_url" => "https://zobirofkir.com",
+        ];
+        $response = $this->post('api/auth/user/reels', $data);
+
+        $response->assertStatus(201);
     }
 }
