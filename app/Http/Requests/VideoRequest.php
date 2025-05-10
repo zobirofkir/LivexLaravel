336560e84617a02f772c1a7b0a180b4a40497715
csw <zobirofkir19@gmail.com>
2025-05-10 06:00:50 +0100

    create video request

---
+++ app/Http/Requests/VideoRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VideoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "title" => "required|string|max:255",
+            "video_url" => "required|url",
+            "thumbnail" => "nullable|image|mimes:jpeg,png,jpg,gif|max:2048",
+            "description" => "nullable|string|max:1000",
+            "duration" => "nullable|integer|min:0",
+            "views" => "nullable|integer|min:0",
+        ];
+    }
+}
