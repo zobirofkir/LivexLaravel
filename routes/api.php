643426089802400627ef22b470b8a63ff2bee570
csw <zobirofkir19@gmail.com>
2025-05-16 15:17:25 +0100

    clean api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,22 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+/**
+ * authenticated routes
+ */
 require __DIR__.'/auth/authenticated.php';
+
+/**
+ * live Routes
+ */
+require __DIR__.'/auth/live.php';
+
+/**
+ * mail routes
+ */
 require __DIR__.'/auth/mail.php';
+
+/**
+ * phone routes
+ */
 require __DIR__.'/auth/phone.php';
