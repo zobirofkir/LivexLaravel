314acd90b46845072382fb67e4e8b8afedd25030
csw <zobirofkir19@gmail.com>
2025-05-16 15:46:05 +0100

    create ralation sheep

--- app/Models/Live.php
+++ app/Models/Live.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Live extends Model
 {
+    /**
+     * Filables
+     */
     protected $fillable = [
         'user_id',
         'stream_key',
@@ -15,4 +20,13 @@ class Live extends Model
         'viewer_count',
         'likes_count'
     ];
+
+    /**
+     * User Relation Sheep
+     */
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
