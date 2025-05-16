7b703fd69de0c43a8ad579095fad4cf77e773f75
csw <zobirofkir19@gmail.com>
2025-05-16 15:44:31 +0100

    create fillables for live model

--- app/Models/Live.php
+++ app/Models/Live.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Live extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'stream_key',
+        'title',
+        'started_at',
+        'ended_at',
+        'viewer_count',
+        'likes_count'
+    ];
 }
