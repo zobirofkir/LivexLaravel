f9be17e571a42e7a0e7e45fa8318681f0dd20e63
csw <zobirofkir19@gmail.com>
2025-07-08 21:49:33 +0100

    download filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthUserServiceProvider::class,
     App\Providers\EmailAuthServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\LiveServiceProvider::class,
     App\Providers\LiveStreamServiceProvider::class,
     App\Providers\PhoneAuthServiceProvider::class,
