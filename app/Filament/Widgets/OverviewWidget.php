7eb1acfa821dc7c92672a4697e1aa4ffe7e06264
csw <zobirofkir19@gmail.com>
2025-07-11 14:51:09 +0100

    create state overview

--- app/Filament/Widgets/OverviewWidget.php
+++ app/Filament/Widgets/OverviewWidget.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
+use App\Models\LiveStream;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,7 +12,20 @@ class OverviewWidget extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Total Users', User::count())
+                ->description('Registered users')
+                ->descriptionIcon('heroicon-m-users')
+                ->color('success'),
+            
+            Stat::make('Total Live Streams', LiveStream::count())
+                ->description('All live streams')
+                ->descriptionIcon('heroicon-m-video-camera')
+                ->color('info'),
+            
+            Stat::make('Currently Live', LiveStream::where('is_live', true)->count())
+                ->description('Active streams')
+                ->descriptionIcon('heroicon-m-signal')
+                ->color('danger'),
         ];
     }
 }
