869a7a33eeb5fc922f68ec9eb33bdca82bb72678
csw <zobirofkir19@gmail.com>
2025-07-11 14:35:21 +0100

    add

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
 
-    protected static ?string $navigationGroup = 'User Management';
+    protected static ?string $navigationGroup = 'Management';
 
     public static function form(Form $form): Form
     {
