e7e245dcd09f484873f94b869dc11d6051838179
csw <zobirofkir19@gmail.com>
2025-07-15 11:47:50 +0100

    clean code

--- routes/auth/offers.php
+++ routes/auth/offers.php
@@ -4,13 +4,26 @@ use App\Http\Controllers\Api\OfferController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
-    // User's own offers
+    
+    /**
+     * User's own offers
+     */
     Route::get('/my-offers', [OfferController::class, 'myOffers']);
     
-    // CRUD operations for offers
+    /**
+     * CRUD operations for offers
+     */
     Route::apiResource('offers', OfferController::class);
 });
 
-// Public routes
+/**
+ * Public routes
+ * 
+ * list all offers
+ */
 Route::get('/offers', [OfferController::class, 'index']);
+
+/**
+ * List just one offer
+ */
 Route::get('/offers/{offer}', [OfferController::class, 'show']);
