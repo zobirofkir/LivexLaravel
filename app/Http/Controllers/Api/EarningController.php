5502b625c276ab8dd0a9ada62db90f17d38b3caf
csw <zobirofkir19@gmail.com>
2025-07-23 00:43:27 +0100

    clean earning controller

--- app/Http/Controllers/Api/EarningController.php
+++ app/Http/Controllers/Api/EarningController.php
@@ -4,28 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Earning\StoreEarningRequest;
+use App\Http\Resources\EarningResource;
 use App\Services\Facades\EarningFacade;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Auth;
 
 class EarningController extends Controller
 {
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         $user = Auth::user();
         $earnings = EarningFacade::getEarningHistory($user);
         $totalEarnings = EarningFacade::getTotalEarnings($user);
 
-        return response()->json([
-            'earnings' => $earnings,
-            'total_earnings' => $totalEarnings,
-        ]);
+        return EarningResource::collection($earnings)
+            ->additional(['total_earnings' => $totalEarnings]);
     }
 
-    public function store(StoreEarningRequest $request)
+    public function store(StoreEarningRequest $request): EarningResource
     {
         $user = Auth::user();
         $earning = EarningFacade::addEarning($user, $request->amount, $request->source);
 
-        return response()->json($earning, 201);
+        return new EarningResource($earning);
     }
 }
