e7a8229ee4eb82ce032d4958ab217f60136733d8
csw <zobirofkir19@gmail.com>
2025-07-23 01:13:48 +0100

    clean like controller

--- app/Http/Controllers/Api/LikeController.php
+++ app/Http/Controllers/Api/LikeController.php
@@ -9,6 +9,7 @@ use App\Models\Video;
 use App\Services\Services\LikeService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\JsonResponse;
+use App\Http\Resources\LikeResource; 
 
 class LikeController extends Controller
 {
@@ -22,70 +23,42 @@ class LikeController extends Controller
     public function like(Video $video): JsonResponse
     {
         $user = Auth::user();
-        
-        if (!$user) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
-        }
-        
+                
         $like = $this->likeService->like($user, $video);
         
-        if ($like->wasRecentlyCreated) {
-            return response()->json(['message' => 'Video liked successfully', 'liked' => true], 201);
-        } else {
-            return response()->json(['message' => 'Video was already liked', 'liked' => true], 200);
-        }
+        return response()->json(LikeResource::make($like));
     }
 
     public function unlike(Video $video): JsonResponse
     {
         $user = Auth::user();
         
-        if (!$user) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
-        }
-
         $result = $this->likeService->unlike($user, $video);
 
-        if ($result) {
-            return response()->json(['message' => 'Video unliked successfully'], 200);
-        } else {
-            return response()->json(['message' => 'Video was not liked'], 200);
-        }
+        return response()->json($result);
     }
 
     public function likedVideos(): JsonResponse
     {
         $user = Auth::user();
-        
-        if (!$user) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
-        }
-        
+                
         $likedVideos = $this->likeService->getLikedVideos($user);
-        return response()->json($likedVideos);
+        return response()->json(LikeResource::collection($likedVideos));
     }
 
     public function totalLikes(): JsonResponse
     {
         $user = Auth::user();
         
-        if (!$user) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
-        }
-        
         $totalLikes = $this->likeService->getTotalLikes($user);
-        return response()->json(['total_likes' => $totalLikes]);
+        return response()->json($totalLikes);
     }
 
     public function isLiked(Video $video): JsonResponse
     {
-        $user = Auth::user();
-        
-        if (!$user) {
-            return response()->json(['message' => 'Unauthenticated'], 401);
-        }
+        $user = Auth::user();        
         
         $isLiked = $this->likeService->isLiked($user, $video);
-        return response()->json(['is_liked' => $isLiked]);
+        return response()->json($isLiked);
     }
 }
