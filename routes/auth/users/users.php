d67e0fa5a67d10164bf72a6561fafead8b1eb4c0
csw <zobirofkir19@gmail.com>
2025-07-24 14:20:38 +0100

    update

--- routes/auth/users/users.php
+++ routes/auth/users/users.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\api_v1\users\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->group(function () {
+Route::middleware('auth:api')->prefix('auth')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
 });
