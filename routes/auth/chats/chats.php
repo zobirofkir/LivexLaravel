24ea8f77fb35204eeb68120226fecfb5dae35bca
csw <zobirofkir19@gmail.com>
2025-07-30 01:11:04 +0100

    create chats routes

--- routes/auth/chats/chats.php
+++ routes/auth/chats/chats.php
@@ -1,1 +1,17 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
+
+Route::middleware('auth:api')->group(function () {
+    
+    /**
+     * Route to send a message
+     */
+    Route::post('/messages/send', [MessageController::class, 'sendMessage'])->name('messages.send');
+
+    /**
+     * Route to get all messages for the authenticated user
+     */
+    Route::get('/messages', [MessageController::class, 'getMessages'])->name('messages.index');
+});
