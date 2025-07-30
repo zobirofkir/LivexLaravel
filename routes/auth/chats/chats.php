20bb9d2bc81b2932758cd931224ecdbb69b8be39
csw <zobirofkir19@gmail.com>
2025-07-30 03:10:09 +0100

    change

--- routes/auth/chats/chats.php
+++ routes/auth/chats/chats.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Events\MessageSent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessageController;
+use App\Models\Message;
 
 Route::middleware('auth:api')->prefix("auth/chats")->group(function () {
     
@@ -14,4 +16,20 @@ Route::middleware('auth:api')->prefix("auth/chats")->group(function () {
      * Route to get all messages for the authenticated user
      */
     Route::get('/messages', [MessageController::class, 'getMessages'])->name('messages.index');
+});
+
+/**
+ * Test route to trigger a broadcast event for a message sent event
+ */
+
+Route::get('/test-broadcast', function () {
+    $message = Message::create([
+        'sender_id' => 1, 
+        'receiver_id' => 2, 
+        'content' => 'This is a test message',
+    ]);
+
+    broadcast(new MessageSent($message))->toOthers();
+
+    return response()->json(['status' => 'Broadcast event triggered']);
 });
