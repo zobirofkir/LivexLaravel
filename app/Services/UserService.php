0f06ce68ca9b80542c390b8c7e58803f7ecebd27
csw <zobirofkir19@gmail.com>
2025-07-30 15:30:09 +0100

    list all users except current auth user and admin user

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,11 +3,22 @@
 namespace App\Services;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserService
 {
     public function getAllUsersOrderedByDesc()
     {
         return User::orderBy('created_at', 'desc')->get();
+    }
+
+    public function getAllUsersExceptCurrentAndAdmin()
+    {
+        $currentUserId = Auth::id();
+        return User::where('id', '!=', $currentUserId)
+                    ->where('name', '!=', 'livex')
+                    ->where('email', '!=', 'admin@livex.com')
+                    ->orderBy('created_at', 'desc')
+                    ->get();
     }
 }
