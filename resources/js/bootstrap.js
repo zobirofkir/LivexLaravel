13e0b287be95597c756bd5d07751c78dbe401a58
csw <zobirofkir19@gmail.com>
2025-07-30 03:07:42 +0100

    add broadcasting

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,14 @@
 import axios from 'axios';
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    forceTLS: true
+});
