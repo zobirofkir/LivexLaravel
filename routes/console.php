878374dcac8c250e8e497a6870c303ff05fc0b0d
csw <zobirofkir19@gmail.com>
2025-08-19 15:01:40 +0100

    clean

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,14 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+/**
+ * Display an inspiring quote at random
+ */
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Schedule automatic offer expiration
+/**
+ * Schedule automatic offer expiration
+ */
 Schedule::command('offers:expire')->daily();
