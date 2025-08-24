941c97521332446c36473ad3249674a9e6d8c1b1
csw <zobirofkir19@gmail.com>
2025-08-24 12:40:58 +0100

    create coin model

--- app/Models/Coin.php
+++ app/Models/Coin.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Coin extends Model
 {
-    //
+    protected $fillable = [
+        "user_id",
+        "price",
+        "old_price",
+    ];
+
+    /**
+     * Create Relationship with User Model
+     */
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
