62b915a34fa16c18662cd0bd3f49720ef6c42693
csw <zobirofkir19@gmail.com>
2025-08-24 12:50:43 +0100

    create

--- app/Http/Controllers/CoinController.php
+++ app/Http/Controllers/CoinController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Facades\CoinFacade;
 use Illuminate\Http\Request;
 
 class CoinController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CoinFacade::index();
+    }
 }
