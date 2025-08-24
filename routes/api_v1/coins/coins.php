74666a98c35e272e4e9b004ec7a1b0f18ca49b15
csw <zobirofkir19@gmail.com>
2025-08-24 12:53:14 +0100

    fix error

--- routes/api_v1/coins/coins.php
+++ routes/api_v1/coins/coins.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\CoinController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->prefix("auth/chats")->group(function () {
+Route::middleware('auth:api')->prefix("auth")->group(function () {
     Route::apiResource('coins', CoinController::class);
 });
