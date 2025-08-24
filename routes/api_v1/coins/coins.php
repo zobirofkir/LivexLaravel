5abc2a39f9c33d84a8d1cfe0b6d9c944405692cb
csw <zobirofkir19@gmail.com>
2025-08-24 12:48:02 +0100

    create couns

--- routes/api_v1/coins/coins.php
+++ routes/api_v1/coins/coins.php
@@ -1,1 +1,8 @@
 <?php
+
+use App\Http\Controllers\CoinController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->prefix("auth/chats")->group(function () {
+    Route::apiResource('coins', CoinController::class);
+});
