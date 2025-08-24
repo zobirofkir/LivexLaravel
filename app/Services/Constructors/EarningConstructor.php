60118773a3608fb94778e30d3867d437bcb85fb7
csw <zobirofkir19@gmail.com>
2025-08-24 12:25:04 +0100

    fix error

--- app/Services/Constructors/EarningConstructor.php
+++ app/Services/Constructors/EarningConstructor.php
@@ -2,7 +2,10 @@
 
 namespace App\Services\Constructors;
 
+use App\Models\Earning;
+use App\Models\User;
+
 interface EarningConstructor
 {
-    public function addEarning($user, float $amount, string $source, ?string $notes = null, float $platformFeePercentage = 20);
+    public function addEarning(User $user, float $amount, string $source, ?string $notes = null, float $platformFeePercentage = 20): Earning;
 }
