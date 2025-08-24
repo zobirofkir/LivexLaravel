71b8658ffddddc939107cf5772268f7a47c1eeb0
csw <zobirofkir19@gmail.com>
2025-08-24 12:51:20 +0100

    add the coins in coin service

--- app/Providers/CoinServiceProvider.php
+++ app/Providers/CoinServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\Services\CoinService;
 use Illuminate\Support\ServiceProvider;
 
 class CoinServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class CoinServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind("CoinService", function ($app) {
+            return new CoinService();
+        });
     }
 
     /**
