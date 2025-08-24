cd66441a32f4285e3a711381a17ececa123a4953
csw <zobirofkir19@gmail.com>
2025-08-24 12:50:13 +0100

    create coin service

--- app/Services/Services/CoinService.php
+++ app/Services/Services/CoinService.php
@@ -2,9 +2,20 @@
 
 namespace App\Services\Services;
 
+use App\Http\Resources\CoinResource;
+use App\Models\Coin;
 use App\Services\Constructors\CoinConstructor;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CoinService implements CoinConstructor
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return CoinResource::collection(
+            Coin::all()
+        );
+    }
 }
