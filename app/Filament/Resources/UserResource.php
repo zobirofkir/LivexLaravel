d22223eba8664f1bda577832d12754edd79764c1
csw <zobirofkir19@gmail.com>
2025-09-19 22:08:15 +0100

    remove management from filament user resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
 
-    protected static ?string $navigationGroup = 'User Management';
+    protected static ?string $navigationGroup = 'User';
 
     public static function form(Form $form): Form
     {
