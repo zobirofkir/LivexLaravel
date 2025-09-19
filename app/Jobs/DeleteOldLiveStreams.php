d08f988eae7f4b566f780663565c1f4217fe1bc5
csw <zobirofkir19@gmail.com>
2025-09-19 22:07:06 +0100

    delete live streams every 30 seconds

---
+++ app/Jobs/DeleteOldLiveStreams.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\LiveStream;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteOldLiveStreams implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle(): void
+    {
+        LiveStream::where('created_at', '<=', now()->subMinutes(30))->delete();
+    }
+}
